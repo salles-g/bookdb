fa1ac831ecd92dd30ca604dd66df0f58953fa3ac
salles-g <salles.gustavog@gmail.com>
2024-09-01 03:57:25 -0300

    refactor: create SearchForm component

---
+++ src/components/molecules/SearchForm.tsx
@@ -0,0 +1,45 @@
+import { useReads } from "../../context/ReadsProvider";
+import React, { useEffect } from "react";
+import { Form, useNavigation, useSubmit } from "react-router-dom";
+
+const SearchForm = () => {
+  const { q } = useReads();
+  const submit = useSubmit();
+  const navigation = useNavigation();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  useEffect(() => {
+    $("#search-form")!.value = q;
+  }, [q]);
+
+  return (
+    <div>
+      <Form id="search-form" role="search">
+        <input
+          id="q"
+          className={searching ? "loading" : ""}
+          aria-label="Buscar leituras"
+          placeholder="Buscar"
+          type="search"
+          name="q"
+          defaultValue={q}
+          onChange={(event) => {
+            const isFirstSearch = q == null;
+            submit(event.currentTarget.form!, {
+              replace: !isFirstSearch,
+            });
+          }}
+        />
+        <div id="search-spinner" aria-hidden hidden={!searching} />
+        <div className="sr-only" aria-live="polite"></div>
+      </Form>
+      <Form method="post">
+        <button type="submit">New</button>
+      </Form>
+    </div>
+  );
+};
+
+export default SearchForm;
