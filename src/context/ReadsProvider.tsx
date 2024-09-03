37ec051a77ae1c11c1e124752c6bf912a6a86f3d
salles-g <salles.gustavog@gmail.com>
2024-09-03 10:50:32 -0300

    refactor(ReadsProvider): return lists

--- src/context/ReadsProvider.tsx
+++ src/context/ReadsProvider.tsx
@@ -1,45 +1,53 @@
-import { updateReadList } from "../reads";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { updateList as updateReadList } from "../reads";
 
 export const useReads = () => {
   return useContext(ReadsContext)!;
 };
 
+type LoaderData = {
+  q: string;
+  lists: TList[];
+  reads: TRead[];
+};
+
 interface ReadsProviderValue {
   q: string;
   reads: TRead[];
+  lists: TList[];
   previews: TRead[] | null;
   setPreviews: React.Dispatch<React.SetStateAction<TRead[] | null>>;
-  updateList: (newList: TRead[]) => void;
+  updateList: (listId: string, reads: TRead[]) => void;
   updateCount: number;
 }
 const ReadsContext = createContext<ReadsProviderValue | null>(null);
 const ReadsProvider = ({ children }) => {
-  const { reads: list, q } = useLoaderData() as { reads: TRead[]; q: string };
-  const [reads, setReads] = useState<TRead[]>(list);
+  const { q, lists: initialLists, reads } = useLoaderData() as LoaderData;
+  const [lists, setLists] = useState<TList[]>(initialLists);
   const [updateCount, setUpdateCount] = useState<number>(0);
   const [previews, setPreviews] = useState<TRead[] | null>(null);
 
-  const updateList = (newList: TRead[]) => {
-    const orderedList = newList.map((item, index) => ({
-      ...item,
-      order: index,
-    }));
-    setReads(orderedList);
-    updateReadList(orderedList);
-    setUpdateCount((c) => c + 1);
+  const updateList = (listId: string, reads: TRead[]) => {
+    updateReadList(listId, {
+      reads,
+    });
+    setLists((prevLists) =>
+      prevLists.map((list) => (list.id === listId ? { ...list, reads } : list))
+    );
+    setUpdateCount(updateCount + 1);
   };
 
   useEffect(() => {
-    setReads(list);
-  }, [list]);
+    setLists(initialLists);
+  }, [initialLists]);
 
   return (
     <ReadsContext.Provider
       value={{
         q,
         reads,
+        lists,
         updateList,
         previews,
         updateCount,
